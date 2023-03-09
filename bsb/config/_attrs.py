3fb864776e3f1bd628bd147fe347441b0aee44c4
Robin De Schepper <robin.deschepper93@gmail.com>
2023-03-09 12:32:17 +0100

    removed debug traceback print

--- bsb/config/_attrs.py
+++ bsb/config/_attrs.py
@@ -463,7 +463,6 @@ class ConfigurationAttribute:
                 e.node, e.attr = instance, self.attr_name
             raise
         except Exception as e:
-            traceback.print_exc()
             raise CastError(
                 f"Couldn't cast '{value}' into {self.type.__name__}: {e}",
                 instance,
